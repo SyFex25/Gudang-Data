93cb4f1d9c334bb44e5df1c22aa256a52cdc0d71
SyFex25 <129936560+SyFex25@users.noreply.github.com>
2023-11-08 17:54:03 +0700

    Revert "Merge pull request #12 from elisabethviolinaa/main"
    This reverts commit 69692e8cdc958a33816c520849893cc38e6fde2b, reversing
    changes made to c33fe4e785909ce001066afb21d0ade7d6616830.

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,9 @@ sys.path.append(current_dir)
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, request, render_template, redirect, url_for, jsonify
+from flask import Flask, request, render_template, jsonify
 from sqlalchemy import func
-from models import db, DateDimension, ProductDimension, StoreDimension, CashierDimension, PromotionDimension, PaymentMethodDimension, TravellerShopperDimension, RetailSalesFact
+from models import db, ProductDimension, StoreDimension,PromotionDimension, RetailSalesFact
 
 app = Flask(__name__)
 
@@ -20,7 +20,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1/gudang_data_tes
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-# migrate = Migrate(app, db)
 
 @app.route('/')
 def display_chart():
@@ -34,35 +33,12 @@ def display_fact_table():
     retail_sales_facts = RetailSalesFact.query.all()
     return render_template('fact_table.html', retail_sales_facts = retail_sales_facts)
 
-@app.route('/stores')
-def display_stores():
-    return render_template('stores.html')
-
-@app.route('/cashiers')
-def display_cashiers():
-    cashier_dimension = CashierDimension.query.all()
-    return render_template('cashiers.html', cashier_dimension=cashier_dimension)
-
-@app.route('/products')
-def display_products():
-    return render_template('products.html')
-
 @app.route('/promotions')
 def display_promotions():
     promotion_dimension = PromotionDimension.query.all()
-    return render_template('promotions.html', promotion_dimension=promotion_dimension)
-
-@app.route('/query_gross_profit')
-def query_gross_profit():
-    results = RetailSalesFact.query.filter(
-    RetailSalesFact.store_key == "2123",
-    RetailSalesFact.date_key == "2023-11-02",
-    RetailSalesFact.product_key == "1035453804963260181506960"
-    ).with_entities(RetailSalesFact.extended_gross_profit_dollar_amount).all()
-    for a in results:
-        print(a[0])
-
-    return "Done"
+    retail_sales_facts = RetailSalesFact.query.all()
+    product_dimension = ProductDimension.query.all()
+    return render_template('promotions.html', promotion_dimension=promotion_dimension, retail_sales_facts=retail_sales_facts, product_dimension=product_dimension)
 
 @app.route('/gross_margin_data')
 def gross_margin_data():
